1a4a304fc0bf8e6de036be9a5e7f6919dd4a710f
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-24 03:15:07 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,26 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
   end
 
+   
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
 
+  def brand=brand
+    @brand=brand
+    if brand = brand 
+      BRANDS << brand 
+    end 
+  end 
+   
+   #unique brands : brand, size - 3 arguments 
+   
 end
