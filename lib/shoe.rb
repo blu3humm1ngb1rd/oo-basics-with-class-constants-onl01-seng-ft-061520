3ce9ed2412abccfe4f8a52140edbe26756835ca7
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-24 03:24:12 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,9 +6,8 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if BRANDS.none? = brand 
-      BRANDS << brand
-    end 
+   BRANDS.none? {|unique_brand| unique_brand = brand}
+    
   end
 
    
